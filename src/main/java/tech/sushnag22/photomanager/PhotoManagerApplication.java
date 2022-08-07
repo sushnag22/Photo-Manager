6431b0ae0a56cc0a740415c7d71775970ced63e5
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-07 22:55:26 +0530

    Initial commit

---
+++ src/main/java/tech/sushnag22/photomanager/PhotoManagerApplication.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.photomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/sushnag22/photomanager/PhotoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.photomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
